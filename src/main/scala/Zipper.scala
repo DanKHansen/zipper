8c5a1860307c4c55a9531e1a8054dac430550663
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-02 12:21:17 +0200

    solution

--- src/main/scala/Zipper.scala
+++ src/main/scala/Zipper.scala
@@ -1,17 +1,16 @@
 import scala.annotation.tailrec
 object Zipper:
-   case class Zipper[A](focus: BinTree[A], up: Option[Zipper[A]], isLeft: Boolean)
-   def fromTree[A](bt: BinTree[A]): Zipper[A] = Zipper(bt, None, false)
+   case class Zipper[A](focus: BinTree[A], up: Option[Zipper[A]] = None, isLeft: Boolean = false)
+   def fromTree[A](bt: BinTree[A]): Zipper[A] = Zipper(bt)
    @tailrec
    def toTree[A](zipper: Zipper[A]): BinTree[A] =
       zipper.up match
          case Some(p) if zipper.isLeft  => toTree(p.copy(p.focus.copy(left = Some(zipper.focus))))
          case Some(p) if !zipper.isLeft => toTree(p.copy(p.focus.copy(right = Some(zipper.focus))))
          case _                         => zipper.focus
-
    def value[A](zipper: Zipper[A]): A = zipper.focus.value
    def left[A](zipper: Zipper[A]): Option[Zipper[A]] = zipper.focus.left.map(ch => Zipper(ch, Some(zipper), true))
-   def right[A](zipper: Zipper[A]): Option[Zipper[A]] = zipper.focus.right.map(ch => Zipper(ch, Some(zipper), false))
+   def right[A](zipper: Zipper[A]): Option[Zipper[A]] = zipper.focus.right.map(ch => Zipper(ch, Some(zipper)))
    def up[A](zipper: Zipper[A]): Option[Zipper[A]] = zipper.up
    def setValue[A](v: A, zipper: Zipper[A]): Zipper[A] = zipper.copy(zipper.focus.copy(value = v))
    def setLeft[A](l: Option[BinTree[A]], zipper: Zipper[A]): Zipper[A] = zipper.copy(zipper.focus.copy(left = l))
