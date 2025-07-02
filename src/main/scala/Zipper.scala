4ebd82dcf3cb6360e77adbdbf75a0a9f8c2824b6
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-02 12:38:09 +0200

    solution

--- src/main/scala/Zipper.scala
+++ src/main/scala/Zipper.scala
@@ -3,17 +3,16 @@ object Zipper:
    case class Zipper[A](focus: BinTree[A], up: Option[Zipper[A]] = None, isLeft: Boolean = false)
    def fromTree[A](bt: BinTree[A]): Zipper[A] = Zipper(bt)
    @tailrec
-   def toTree[A](zipper: Zipper[A]): BinTree[A] =
-      zipper.up match
-         case Some(p) if zipper.isLeft  => toTree(p.copy(p.focus.copy(left = Some(zipper.focus))))
-         case Some(p) if !zipper.isLeft => toTree(p.copy(p.focus.copy(right = Some(zipper.focus))))
-         case _                         => zipper.focus
-   def value[A](zipper: Zipper[A]): A = zipper.focus.value
-   def left[A](zipper: Zipper[A]): Option[Zipper[A]] = zipper.focus.left.map(ch => Zipper(ch, Some(zipper), true))
-   def right[A](zipper: Zipper[A]): Option[Zipper[A]] = zipper.focus.right.map(ch => Zipper(ch, Some(zipper)))
-   def up[A](zipper: Zipper[A]): Option[Zipper[A]] = zipper.up
-   def setValue[A](v: A, zipper: Zipper[A]): Zipper[A] = zipper.copy(zipper.focus.copy(value = v))
-   def setLeft[A](l: Option[BinTree[A]], zipper: Zipper[A]): Zipper[A] = zipper.copy(zipper.focus.copy(left = l))
-   def setRight[A](r: Option[BinTree[A]], zipper: Zipper[A]): Zipper[A] = zipper.copy(zipper.focus.copy(right = r))
-
+   def toTree[A](z: Zipper[A]): BinTree[A] = z.up match
+      case Some(p) => toTree(p.copy(p.focus.copy(
+         left = if(z.isLeft) Some(z.focus) else p.focus.left,
+         right = if(!z.isLeft) Some(z.focus) else p.focus.right)))
+      case _ => z.focus
+   def value[A](z: Zipper[A]): A = z.focus.value
+   def left[A](z: Zipper[A]): Option[Zipper[A]] = z.focus.left.map(Zipper(_, Some(z), true))
+   def right[A](z: Zipper[A]): Option[Zipper[A]] = z.focus.right.map(Zipper(_, Some(z)))
+   def up[A](z: Zipper[A]): Option[Zipper[A]] = z.up
+   def setValue[A](v: A, z: Zipper[A]): Zipper[A] = z.copy(z.focus.copy(value = v))
+   def setLeft[A](l: Option[BinTree[A]], z: Zipper[A]): Zipper[A] = z.copy(z.focus.copy(left = l))
+   def setRight[A](r: Option[BinTree[A]], z: Zipper[A]): Zipper[A] = z.copy(z.focus.copy(right = r))
 case class BinTree[A](value: A, left: Option[BinTree[A]], right: Option[BinTree[A]])
